c474eae448b2e435bb449a87a291482266d51b41
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-26 12:18:17 +0500

    You have six bowls of Rice Krispies in front of you, and they make different noises when you pour milk over them based on the total number of Rice Krispies in each bowl. If a bowl has a number of Rice Krispies that is divisible by 3, it will make a Pop sound. If it is not divisible by 3 but is odd it will make a Snap sound.  If it is not divisible by 3, but it is even, it will make a Crackle sound.
    Task:
    Based on the quantities in each bowl, output the noise that they make.
    
    Input Format:
    You receive 6 integers and each integer represents the number of Rice Krispies in your bowls.
    
    Output Format:
    You should output a string with the sounds made by each bowl separated by spaces in the order that they were input.
    
    Sample Input:
    18
    5
    100
    25
    40
    24
    
    Sample Output:
    Pop Snap Crackle Snap Crackle Pop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String SOUND5 = "Snap";
+        final String SOUND2 = "Crackle";
+        final String SOUND3 = "Pop";
+        String sOut="";
+        int n;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            n=sc.nextInt();
+            if (n%3==0) {
+                sOut+=SOUND3+" ";
+                continue;
+            }
+            if (n%3!=0 && n%2==0 ) {
+                sOut+=SOUND2+" ";
+                continue;
+            }
+            sOut+=SOUND5+" ";s
+        };
+        System.out.println(sOut.trim());
     }
 }
